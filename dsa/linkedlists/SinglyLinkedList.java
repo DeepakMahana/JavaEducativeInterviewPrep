2b7db9551add51bc581964e4e66f79a006b677ee
unknown <deepak.mahana@franchisediscovery.in>
2023-03-02 22:16:26 +0530

    remove duplicates from ll

--- dsa/linkedlists/SinglyLinkedList.java
+++ dsa/linkedlists/SinglyLinkedList.java
@@ -187,6 +187,25 @@ public class SinglyLinkedList<T> {
         return mid.data;
     }
 
+
+    // // Remove duplicates from LinkedList
+    public static <T> void removeDuplicates(SinglyLinkedList<T> list) {
+        SinglyLinkedList<T>.Node current = list.headNode; // will be used for outer loop
+        SinglyLinkedList<T>.Node compare = null;     // will be used for inner loop
+
+        while (current != null && current.nextNode != null) {
+            compare = current;
+            while (compare.nextNode != null) {
+                if (current.data.equals(compare.nextNode.data)) { //check if duplicate
+                    compare.nextNode = compare.nextNode.nextNode;
+                } else {
+                    compare = compare.nextNode;
+                }
+            }
+            current = current.nextNode;
+        }
+    }
+
     public static void main( String args[] ) {
         SinglyLinkedList<Integer> sll = new SinglyLinkedList<Integer>();
         sll.printList();
